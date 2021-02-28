b1d621db5c9d3b381ad4a8bf9e2119877d4db67b
taiawu <taiawu@gmail.com>
2021-02-27 19:59:12 -0800

    updates to label_data(), including ways to make columns character or numeric type, and some basic error-catchers for if a user attempts to change critical columns into incompatible types, such as value, well, or Temperature

--- R/label_data.R
+++ R/label_data.R
@@ -8,6 +8,8 @@
 #' @param drop_empties TRUE or FALSE value, determining whether data from particular wells should be dropped from the output tibble. Defaults to TRUE.
 #' @param .drop_from If drop_empties is set to TRUE, the unquoted name of the column which will be used to perform value-based filtering.
 #' @param .drop_matches A vector containing all values which, if present in the .drop_from column, will trigger the removal of those wells from the output tibble.
+#' @param .make_numeric A vector containing the names of any columns from the layout which, if not already numeric, should be coerced to numeric. Numeric-type columns are automatically detected using readr::parse_guess() in read_plate_layout, so this argument is necessary only if a user wants to override the decision made by readr::parse_guess().
+#'@param .make_character A vector containing the names of any columns from the layout which, if not already of type character, should be coerced to character. Character-type columns are automatically detected using readr::parse_guess() in read_plate_layout, so this argument is necessary only if a user wants to override the decision made by readr::parse_guess().
 #'
 #' @return A tibble, containing all columns present in the input data and layout tibbles, joined into a single, labeled tibble.
 #' @export
@@ -16,17 +18,45 @@ label_data <- function(data,
                        join_by = "well",
                        drop_empties = TRUE,
                        .drop_from = protein,
-                       .drop_matches = c("Empty", "empty")) {
+                       .drop_matches = c("Empty", "empty"),
+                       .make_numeric  = c(),
+                       .make_character = c()) {
+  # join data and layout
   out <- data %>%
     dplyr::left_join( . , layout, by = join_by) %>%
     dplyr::filter()
 
-  if (drop_empties == TRUE) {
-    try( out <- out %>% dplyr::filter(! {{.drop_from}} %in%.drop_matches))
-  }
+  # filter out .drop_matches
+  if (drop_empties == TRUE) { try( out <- out %>% dplyr::filter(! {{.drop_from}} %in%.drop_matches)) }
+
+  # alert users of invalid .make_character and .make_numeric selections
+  if ("value" %in% .make_character)       { cant_change_that("value", ".make_character", "numeric")
+                                            .make_character <- .make_character[!.make_character %in% c("Temperature", "value", "column")]}
+
+  if ("Temperature" %in% .make_character) { cant_change_that("Temperature", ".make_character", "numeric")
+                                            .make_character <- .make_character[!.make_character %in% c("Temperature", "value", "column")]}
+
+   if ("column" %in% .make_character)      { cant_change_that("column", ".make_character", "numeric")
+                                            .make_character <- .make_character[!.make_character %in% c("Temperature", "value", "column")]}
+
+   if ("well" %in% .make_numeric)          { cant_change_that("well", ".make_numeric", "character")
+                                            .make_numeric <- .make_numeric[!.make_numeric %in% c("well", "row")]}
+
+  if ("row" %in% .make_numeric)           { cant_change_that("row", ".make_numeric", "character")
+                                            .make_numeric <- .make_numeric[!.make_numeric %in% c("well", "row")]}
+
+  # ensure no protected columns are present in the .make_character and .make_numeric selections
+
+
+  # perform desired type-changes
+  try(out <- out %>% dplyr::mutate(across(all_of(.make_numeric), base::as.numeric)))
+  try(out <- out %>% dplyr::mutate(across(all_of(.make_character), base::as.character)))
 
   out
+}
 
+cant_change_that <- function( change_what, which_argument, keep_as ) {
+  glue::glue("You have supplied '{change_what}' to the {which_argument} argument. However, the {change_what} column must remain {keep_as}, so no change has been made to the type of the {change_what} column.") %>% print()
 }
 
 utils::globalVariables(c("protein"))
---
+++ R/read_layout.R
@@ -0,0 +1,54 @@
+#' Read a plate layout file into a tibble
+#'
+#'read_layout() reads a plate layout file (.csv, .txt, .xls, or .xlsx), and returns it as a formatted tibble. Variable types are guessed with readr::parse_guess(). The originally required "Type" column heading is now optional.
+#'
+#'
+#'
+#' @param filepath A complete file path, pointing to the plate layout file
+#'
+#' @return Returns a tibble, mapping experimental variables to well positions. All outputs contain the columns: row, column, and well. Additionally, a single column is added for each user-defined variable in the plate layout file, from which one additional "condtion" column is created, which contains all experimental variables. If all experimental variables are defined in the layout, wells with identical entries in the "condition" column are technical replicates.
+#'
+#' @import dplyr
+#' @importFrom magrittr "%>%"
+#' @import purrr
+#' @import readr
+#' @import readxl
+#' @import tidyr
+#' @import tools
+#'
+#'
+#' @export
+
+read_layout <- function(filepath) {
+
+  # read file based on it's type
+  ext <- tools::file_ext(filepath)
+
+  raw <- switch(ext,
+                csv =  readr::read_csv(filepath, col_names = FALSE),
+                txt =  readr::read_tsv(filepath, col_names = FALSE),
+                xlsx = readxl::read_excel(filepath, col_names = FALSE),
+                xls =  readxl::read_excel(filepath, col_names = FALSE)
+  )
+
+  # handle files with or without the "Type" header
+  first_cell <- raw[1,1][[1]]
+  out <- switch(first_cell,
+                Type = raw[-1,],
+                raw)
+
+  # convert into layout form
+  out %>%
+    purrr::set_names( c("variable", "row", .data$.[1,][-c(1,2)])) %>%
+    dplyr::filter(row %in% base::LETTERS[1:16]) %>%
+    purrr::discard(~all(is.na(.data$.x)))  %>% # drop columns if everything is NA
+    dplyr::filter(if_all(everything(), ~ !is.na(.data$.x))) %>%
+    dplyr::mutate(across(everything(), as.character)) %>% # make all character, to prevent issues in pivot
+    tidyr::pivot_longer(-c(.data$variable, .data$row), names_to = "column", values_to = "value") %>%
+    tidyr::pivot_wider(names_from = .data$variable, values_from = .data$value) %>%
+    dplyr::mutate(well = paste0(.data$row, .data$column)) %>% # make well column
+    tidyr::unite(.data$condition, -c(.data$row, .data$column, .data$well), sep = "__", remove = FALSE) %>% # make condition column
+    dplyr::filter(!across(-c(.data$well, .data$row, .data$column, .data$condition)) == "Empty") %>% # if all variables are "Empty",   wells
+    dplyr::filter(!is.na(across(-c(.data$well, .data$row, .data$column, .data$condition))) == TRUE) %>% # if all variables are NA,   wells
+    dplyr::mutate(across(everything(), readr::parse_guess)) # convert likely numeric variables to numeric
+}
