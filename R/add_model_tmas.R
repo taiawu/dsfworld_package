8e0c6ae1249259597c039a209876bdbe81ae3072
taiawu <taiawu@gmail.com>
2021-08-16 23:30:41 -0700

    modify arguments to add_model_tmas(); was failing to handle ... correctly. Look into this further; I'm not sure why it failed in the way that it did.

--- R/add_model_tmas.R
+++ R/add_model_tmas.R
@@ -1,3 +1,37 @@
+#' Extract component tmas
+#'
+#' A helper function for add_model_tmas. Extracts apparent melting temperatures from individually-predicted sigmoids from model fits. Uses maximum of dRFU to do this.
+#'
+#' @param data a tibble, as returned by add_model_preds().
+#' @param component_names the names of the components from which to extract Tmas. Defaults to c("sigmoid_1", "sigmoid_2", "sigmoid_3"). Components listed but not found in data are silently ignored.
+#' @param .x_vec a string, giving the name of the column containing the x data. Passed to get_drfu_tmas.
+#' @param .precision a number, giving the precision by which to iterpolate the drfu values to get drfu tmas. Defaults ot 0.001, which roughly corresponds to one or two decimal places of accuracy in actual temperature, when converted from 0 to 1 normalized temperature used in the fitting.
+#' @param ... additional arguments, passed to get_drfu_tmas. Also serves the purpose of allowing irrelevant arguments passed from upstream functions using ... to be ignored.
+#'
+#' @return a tibble containing two columsn, "which_value", a character defining which of the input component_names the Tma corresponds to, and "tma", the tma value determined using get_drfu_tmas. Note that the tmas will be for the x vector to which the models were fit, which in the standard workflow, is "Temperature_norm". Conversion to real temperatures currently is handled outside of the fitting workflow.
+#'
+#' @export
+function(data,
+         component_names = c("sigmoid_1", "sigmoid_2", "sigmoid_3"),
+         .x_vec = "Temperature_norm",
+         .precision = 0.001,
+         ...) {
+
+  df <- data %>%
+    dplyr::filter(.data$which_value %in% component_names) %>%
+    dplyr::group_by(.data$which_value) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(data = purrr::map(.data$data, ~ dplyr::mutate(.x,
+                                                                value_norm = scales::rescale(.data$value, to = c(0,1)),
+                                                                drfu_norm = sgolay(.data$value_norm, m = 1)
+    )),
+    tma = purrr::map(data, get_drfu_tmas, .x_vec = "Temperature_norm", .precision = 0.001, ...)[[1]])
+
+  out <- df %>%
+    dplyr::ungroup() %>%
+    dplyr::select(.data$which_value, .data$tma)
+}
+
 #' Add dRFU tmas to nested dataframe
 #'
 #' @param by_var  nested tibble, as output by add_model_preds()
@@ -13,33 +47,4 @@ add_model_tmas <-
       dplyr::mutate(model_tma = try( purrr::map(.data$data, extract_component_tma, ...)))
   }
 
-#' Extract component tmas
-#'
-#' A helper function for add_model_tmas. Extracts apparent melting temperatures from individually-predicted sigmoids from model fits. Uses maximum of dRFU to do this.
-#'
-#' @param data a tibble, as returned by add_model_preds().
-#' @param component_names the names of the components from which to extract Tmas. Defaults to c("sigmoid_1", "sigmoid_2", "sigmoid_3"). Components listed but not found in data are silently ignored.
-#' @param ... additional arguments, passed to get_drfu_tmas. Also serves the purpose of allowing irrelevant arguments passed from upstream functions using ... to be ignored.
-#'
-#' @return a tibble containing two columsn, "which_value", a character defining which of the input component_names the Tma corresponds to, and "tma", the tma value determined using get_drfu_tmas. Note that the tmas will be for the x vector to which the models were fit, which in the standard workflow, is "Temperature_norm". Conversion to real temperatures currently is handled outside of the fitting workflow.
-#'
-#' @export
-extract_component_tma <-
-  function(data,
-           component_names = c("sigmoid_1", "sigmoid_2", "sigmoid_3"),
-           ...) {
 
-    df <- data %>%
-      dplyr::filter(.data$which_value %in% component_names) %>%
-      dplyr::group_by(.data$which_value) %>%
-      tidyr::nest() %>%
-      dplyr::mutate(data = purrr::map(.data$data, ~ dplyr::mutate(.x,
-                                                                  value_norm = scales::rescale(.data$value, to = c(0,1)),
-                                                                  drfu_norm = sgolay(.data$value_norm, m = 1)
-      )),
-      tma = purrr::map(data, get_drfu_tmas, ...)[[1]])
-
-    out <- df %>%
-      dplyr::ungroup() %>%
-      dplyr::select(.data$which_value, .data$tma)
-  }
