605f8ac3c6778bd1e154bcd208ee46571379d68f
taiawu <taiawu@gmail.com>
2023-12-15 16:07:53 -0800

    check 0-0-0; additions to .Rbuildignore, removal of unnecessary problematic code in vignette and tests

--- tests/testthat/test-read-plate-layout.R
+++ tests/testthat/test-read-plate-layout.R
@@ -1,8 +1,10 @@
-#------- Correct output format ---------
-test_that("read_plate_layout returns a tibble", {
-  test_data <- read_plate_layout(test_path("raw_plate_layout.csv"))
-  expect_s3_class(test_data, "tbl_df")
-})
+## remove this test, because this function has been moved to tidyplate(), and has a passing test there.
+
+# #------- Correct output format ---------
+# test_that("read_plate_layout returns a tibble", {
+#   test_data <- read_plate_layout(test_path("raw_plate_layout.csv"))
+#   expect_s3_class(test_data, "tbl_df")
+# })
 
 # has correct column names
 # has correct column types
